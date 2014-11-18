6ad2212184cd35961a6d6d8d3af59e9af4ae352f
John Otander <johnotander@gmail.com>
2014-11-17 19:00:39 -0700

    Break url-regex into a util

--- addon/helpers/linkify.js
+++ addon/helpers/linkify.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
+import urlRegex from 'ember-linkify/utils/url-regex';
 
 export function linkify(textToLinkify) {
   textToLinkify = Ember.Handlebars.Utils.escapeExpression(textToLinkify);
 
-  // URL regex courtesy of https://github.com/kevva/url-regex
-  textToLinkify = textToLinkify.replace(/(?:^|\s)(["'])?(?:(?:(?:(?:https?|ftp|\w):)?\/\/)|(?:www.))(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?\1/ig, function (s) {
+  textToLinkify = textToLinkify.replace(urlRegex(), function (s) {
     return ' <a href="' + s.trim() + '">' + s.trim() + '</a> ';
   });
 
