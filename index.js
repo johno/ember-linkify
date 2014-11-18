3ac2f26b15629e63466319b9e85c0f9ef0dc2db1
John Otander <johnotander@gmail.com>
2014-11-17 20:20:20 -0700

    Remove unneeded blueprint path

--- index.js
+++ index.js
@@ -3,9 +3,5 @@
 var path = require('path');
 
 module.exports = {
-  name: 'ember-linkify',
-
-  blueprintsPath: function() {
-    return path.join(__dirname, 'blueprints');
-  },
+  name: 'ember-linkify'
 };
