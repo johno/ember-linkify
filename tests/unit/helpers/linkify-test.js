227bad33d09ba284371d24c0473e918381a801e1
John Otander <johnotander@gmail.com>
2014-11-17 19:15:17 -0700

    Add tests for linkify helper

--- tests/unit/helpers/linkify-test.js
+++ tests/unit/helpers/linkify-test.js
@@ -4,8 +4,22 @@ import {
 
 module('LinkifyHelper');
 
-// Replace this with your real tests.
-test('it works', function() {
-  var result = linkify(42);
-  ok(result);
+test('it should turn a url into a link', function() {
+  var result = linkify('My link: http://google.com').toString().trim();
+  equal(result, 'My link: <a href="http://google.com">http://google.com</a>');
+});
+
+test('it should turn a url with www. into a link', function() {
+  var result = linkify('www.johnotander.com').toString().trim();
+  equal(result, '<a href="www.johnotander.com">www.johnotander.com</a>');
+});
+
+test('it should escape html', function() {
+  var result = linkify('<h1>Some Html</h1>').toString().trim();
+  equal(result, '&lt;h1&gt;Some Html&lt;/h1&gt;');
+});
+
+test('it should not allow script tags because bad', function() {
+  var result = linkify('<script>alert("oh noes!");</script>').toString().trim();
+  equal(result, '&lt;script&gt;alert(&quot;oh noes!&quot;);&lt;/script&gt;');
 });
