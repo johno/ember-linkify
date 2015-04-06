78031bb52a64254f966052d7f250b6f3457e37b7
Kris Hedges <khedges@switchboxinc.com>
2015-04-06 14:05:50 -0400

    Fix no target provided errors. Apparently target in “this” context here is some other object so renamed it.

--- addon/helpers/linkify.js
+++ addon/helpers/linkify.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 import urlRegex from 'ember-linkify/utils/url-regex';
 
-export function linkify(textToLinkify, target) {
-  target = target || "_self";
+export function linkify(textToLinkify, windowTarget) {
+  windowTarget = windowTarget || "_self";
   textToLinkify = Ember.Handlebars.Utils.escapeExpression(textToLinkify);
 
   textToLinkify = textToLinkify.replace(urlRegex(), function (s) {
-    return ' <a href="' + s.trim() + '" target="'+target+'">' + s.trim() + '</a> ';
+    return ' <a href="' + s.trim() + '" target="'+windowTarget+'">' + s.trim() + '</a> ';
   });
 
   return new Ember.Handlebars.SafeString(textToLinkify);
