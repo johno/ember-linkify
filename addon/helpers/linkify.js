d725d409330cda10a7806fdd2336e555e184c484
Bhushan <Bhushan@Bhushans-MBP-2.fritz.box>
2016-04-06 20:00:50 +0200

    Add URL shortener logic : passed as an option to the helper

--- addon/helpers/linkify.js
+++ addon/helpers/linkify.js
@@ -1,18 +1,26 @@
 import Ember from 'ember';
-import urlRegex from 'ember-linkify/utils/url-regex';
+import { urlRegex , shortenUrl } from 'ember-linkify/utils/url-regex';
 
-export function linkify(textToLinkify, windowTarget) {
-  windowTarget = windowTarget || "_self";
-  textToLinkify = Ember.Handlebars.Utils.escapeExpression(textToLinkify);
+export function linkify( params, options ) {
+  
+  var textToLinkify = Ember.Handlebars.Utils.escapeExpression(params[0]);
+  var windowTarget = params[1] || "_self";
 
   textToLinkify = textToLinkify.replace(urlRegex(), function (s) {
     var url;
+    var displayText = s.trim();
+
     if(s.trim().match(/^www\./ig)) {
       url = '//' + s.trim();
     } else {
       url = s.trim();
     }
-    return ' <a href="' + url + '" target="' + windowTarget + '">' + s.trim() + '</a> ';
+
+    if( options && options.urlLength &&  options.urlLength > 0 ) {
+      displayText = shortenUrl( displayText, options.urlLength ); 
+    } 
+
+    return ' <a href="' + url + '" target="' + windowTarget + '">' + displayText + '</a> ';
   });
 
   return Ember.String.htmlSafe(textToLinkify);
--- addon/utils/url-regex.js
+++ addon/utils/url-regex.js
@@ -1,5 +1,18 @@
-// URL regex courtesy of https://github.com/kevva/url-regex
+import Ember from 'ember';
 
-export default function () {
+// URL regex courtesy of https://github.com/kevva/url-regex
+function urlRegex () {
   return /(?:^|\s)(["'])?(?:(?:(?:(?:https?|ftp|\w):)?\/\/)|(?:www.))(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?\1/ig;
 }
+
+// Shortens the URL and adds three dots to the end
+function shortenUrl ( url , length ) {
+  
+  if( !Ember.isBlank( url ) ) {
+      url = url.substr( 0 , length ) + "..."; 
+  }
+
+  return url;
+}
+
+export { urlRegex, shortenUrl };
