ec9fbc2dce13dcf4af29888ce479a6fec5422778
Nate Whittaker <nwhittaker@esri.com>
2016-05-07 23:19:33 -0400

    "rel" attribute
    Add support for specifying the “rel" attribute.

--- addon/helpers/linkify.js
+++ addon/helpers/linkify.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 import { urlRegex , shortenUrl } from 'ember-linkify/utils/url-regex';
 
+var ALLOWED_ATTRIBUTE_NAMES = [ 'rel' ];
+
 export function linkify( params, options ) {
   
   var textToLinkify = Ember.Handlebars.Utils.escapeExpression(params[0]);
   var windowTarget = params[1] || "_self";
+  var sharedAttributes = opts2attrs( options );
 
   textToLinkify = textToLinkify.replace(urlRegex(), function (s) {
     var url;
@@ -20,10 +23,24 @@ export function linkify( params, options ) {
       displayText = shortenUrl( displayText, options.urlLength ); 
     } 
 
-    return ' <a href="' + url + '" target="' + windowTarget + '">' + displayText + '</a> ';
+    return ' <a href="' + url + '" target="' + windowTarget + '"' + sharedAttributes + '>' + displayText + '</a> ';
   });
 
   return Ember.String.htmlSafe(textToLinkify);
 }
 
 export default Ember.Helper.helper(linkify);
+
+function opts2attrs( options ) {
+  var stringOfAttributes = '';
+
+  if( typeof options === 'object' ) {
+    for( var i = ALLOWED_ATTRIBUTE_NAMES.length - 1, attributeName = ALLOWED_ATTRIBUTE_NAMES[ i ]; i > -1; i-- ) {
+      if( attributeName in options ) {
+        stringOfAttributes += ' ' + attributeName + '="' + options[ attributeName ] + '"';
+      }
+    }
+  }
+
+  return stringOfAttributes;
+}
